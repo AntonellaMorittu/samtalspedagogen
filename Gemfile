72267cf62ed361b060c806fbd5f5589fae672026
Antonella Morittu <antonella@technocreatives.com>
2018-10-30 16:56:29 +0100

    git init

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'https://rubygems.org'
+ruby "~> 2.5.1"
+
+gem 'middleman', '~> 4.2'
+gem 'middleman-autoprefixer', '~> 2.7'
+gem 'middleman-deploy', '= 2.0.0.pre.alpha'
+gem 'middleman-livereload'
+gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
+gem 'wdm', '~> 0.1', platforms: [:mswin, :mingw]
+gem 'rake'
+gem 'puma'
+gem 'rack-contrib'
+
+group :development, :test do
+  gem 'capybara'
+  gem 'pry'
+  gem 'pry-byebug'
+  gem 'rspec'
+end
---
+++ config.rb
@@ -0,0 +1,46 @@
+require 'extensions/build_cleaner'
+require 'middleman/rack'
+
+configure :build do
+  activate :build_cleaner
+  activate :directory_indexes
+  activate :asset_hash
+end
+
+activate :autoprefixer do |prefix|
+  prefix.browsers = "last 2 versions"
+  activate :livereload
+end
+
+activate :deploy do |deploy|
+  deploy.build_before = true
+  deploy.deploy_method = :git
+end
+
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+# With alternative layout
+# page '/path/to/file.html', layout: 'other_layout'
+
+# Proxy pages
+# https://middlemanapp.com/advanced/dynamic-pages/
+
+# proxy(
+#   '/this-page-has-no-template.html',
+#   '/template-file.html',
+#   locals: {
+#     which_fake_page: 'Rendering a fake page with a local variable'
+#   },
+# )
+
+# Helpers
+# Methods defined in the helpers block are available in templates
+# https://middlemanapp.com/basics/helper-methods/
+
+# helpers do
+#   def some_helper
+#     'Helping'
+#   end
+# end
